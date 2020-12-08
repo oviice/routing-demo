88aa30e8b5501ffe635a91f8c3bf53a9bfd577af
Ovi <oviice@gmail.com>
2020-12-08 18:59:13 +0600

    routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import About from "./mycomponent/About";
+import Contact from "./mycomponent/Contact";
+import Profile from "./mycomponent/Profile";
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+} from "../node_modules/react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
+
+const myRouter = (
+  <Router>
+    <div style={{backgroundColor:"red"}}>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/profile">Profile</Link>
+      </ul>
+
+      <Route exact path="/" component={App}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      <Route path="/profile" component={Profile}/>
+    </div>
+  </Router>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  myRouter,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
